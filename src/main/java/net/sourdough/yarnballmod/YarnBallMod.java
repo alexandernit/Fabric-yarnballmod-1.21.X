750a47bff6e99e3838f32e00de2973cfc1856ae7
Alexander Nitzschke <alexander.nitzschke@yahoo.com>
2025-11-09 19:33:25 +0100

    Initial commit

---
+++ src/main/java/net/sourdough/yarnballmod/YarnBallMod.java
@@ -0,0 +1,16 @@
+package net.sourdough.yarnballmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class YarnBallMod implements ModInitializer {
+	public static final String MOD_ID = "yarnballmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/sourdough/yarnballmod/YarnBallmodClient.java
@@ -0,0 +1,10 @@
+package net.sourdough.yarnballmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YarnBallmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
